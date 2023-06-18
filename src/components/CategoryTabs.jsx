0f5b4c8472e448bd9259a07a22dba64c991e0e05
sfshovon <shahriar.shovon@northsouth.edu>
2023-06-19 03:56:25 +0600

    Hero Section Added

--- src/components/CategoryTabs.jsx
+++ src/components/CategoryTabs.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client"
+import Link from "next/link";
+import { useState } from "react";
 
 const CategoryTabs = () => {
-  return (
-    <div>CategoryTabs</div>
-  )
-}
+  const categories = [
+    "smartphones", "laptops", "fragrances", "skincare", "groceries", "home-decoration",
+    "furniture", "tops", "womens-dresses", "womens-shoes", "mens-shirts", "mens-shoes",
+    "mens-watches", "womens-watches", "womens-bags", "womens-jewellery", "sunglasses",
+    "automotive", "motorcycle", "lighting"
+  ];
 
-export default CategoryTabs
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const getCategoryName = (category) => {
+    let modifiedName = category.replace(/-/g, " "); // Replace all hyphens with spaces
+    modifiedName = modifiedName.replace(/(^|\s)\S/g, (match) =>
+      match.toUpperCase()
+    ); // Capitalize the first letter of each word
+
+    return modifiedName;
+  };
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <ul className="menu menu-vertical lg:menu-horizontal">
+        {categories.map((category, index) => (
+          <li key={index}>
+            <Link
+              href={`#${category}`}
+              className={`px-4 py-2 text-sm font-medium ${
+                selectedCategory === category
+                  ? "text-gray-800 border-b-2 border-gray-800"
+                  : "text-gray-800 hover:text-gray-600"
+              } transition duration-300 ease-in-out`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {getCategoryName(category)}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryTabs;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,39 @@
-import React from "react";
 import CategoryTabs from "./CategoryTabs";
-import HeroBg from "../../public/Hero-bg.png";
 
 const Hero = () => {
   return (
-    <div
-      className="bg-cover bg-no-repeat bg-center bg-[url('../../public/Hero-bg.png')]"
-      style={{ height: "700px" }}
-    >
+    <div className="px-4 md:px-32 bg-cover bg-no-repeat bg-center bg-[url('../../public/Hero-bg.png')] min-h-[1000px] md:min-h-[800px]">
       <CategoryTabs></CategoryTabs>
-      Hero
+      <div className="mt-8 md:mt-24 grid grid-cols-1 md:grid-cols-12 gap-2">
+        <div className="md:col-span-5 text-center md:text-start">
+          <h5 className="text-sm md:text-md font-semibold font-sans mb-3 md:mb-6">100% QUALITY, 100% SATISFACTION</h5>
+          <h1 className="w-full md:w-[504px] text-3xl md:text-6xl font-extrabold font-sans mb-3 md:mb-6">Where the world comes to shop</h1>
+          <p className="w-full md:w-[440px] text-justify text-base md:text-lg font-thin">Life is hard enough already. Let us make it a little easier, Vision of Snipshop for a better outlook.</p>
+          <div className="flex justify-center md:justify-start ">
+            <button className="mt-4 md:mt-8 w-full md:w-[200px] btn btn-neutral py-2 px-8 rounded-full font-semibold">Shop Now</button>
+          </div>
+        </div>
+
+        <div className="md:col-span-4 mt-2 md:mt-0">
+          <div className="flex justify-center md:items-center items-start">
+            <img src="/camera.png" className="w-[240px] md:w-[390px] h-auto" alt="Camera" />
+          </div>
+          <div className="mt-2">
+            <h4 className="text-xs text-center md:text-start md:text-md">Start From</h4>
+            <h3 className="text-lg md:text-2xl text-center md:text-start font-bold">110.00$</h3>
+          </div>
+        </div>
+        <div className="md:col-span-3 mt-2 md:mt-0">
+          <div className="flex flex-col justify-center items-center">
+            <img src="/tripod.png" className="w-[80px] md:w-[130px] h-auto" alt="Camera" />
+            <h6 className="mt-2 md:mt-4 text-xs md:text-sm">Full 14mm Lens</h6>
+          </div>
+          <div className="mt-6 flex flex-col justify-center items-center">
+            <img src="/lens.png" className="w-[80px] md:w-[130px] h-auto" alt="Camera" />
+            <h6 className="mt-2 md:mt-4 text-xs md:text-sm">Camera Stand</h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
