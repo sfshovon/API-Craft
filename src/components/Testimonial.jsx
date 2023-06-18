d9b853b8d26c8c91652d0d53d4a220a2daa58cf2
sfshovon <shahriar.shovon@northsouth.edu>
2023-06-18 23:12:34 +0600

    Testimonial Background Added

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -38,11 +38,10 @@ const Testimonial = () => {
 
   return (
     <div className="flex flex-col gap-8 md:flex-row md:items-center md:gap-14 my-24">
-      <div
-        className="py-5 px-4 flex justify-end w-full md:w-[666px]"
-        style={{ backgroundImage: "url('base.png')", backgroundSize: 'cover' }}
-      >
-        <img className="border border-zinc-400 rounded-3xl w-[440px] h-[530px]" src={testimonial.imageSrc} alt="image" />
+      <div className="py-5 px-4 flex justify-end w-full md:w-[666px]" style={{backgroundImage: "url('base.png')", backgroundSize: 'cover'}}>
+        <div className="border border-sky-200 rounded-3xl w-[440px] h-[530px]" style={{backgroundImage: "url('testimonial-vector.png')", backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+          <img src={testimonial.imageSrc} alt="image" className="w-full h-full" />
+        </div>
       </div>
       <div className="w-full md:w-[463px]">
         <div className="card">
